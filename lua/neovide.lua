c5d9b549bb3ed085b2129f6a5cd436779264ed6e
Harsh Devisha <harshdevisha9111@gmail.com>
2025-01-11 14:54:41 -0800

    refactor(neovide): new settings

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -3,8 +3,15 @@ local vim = vim
 if vim.g.neovide then
 	vim.o.guifont = "JetBrainsMono Nerd Font Mono"
 	vim.opt.linespace = 0
-	vim.g.neovide_scale_factor = 0.75
+	vim.g.neovide_scale_factor = 0.80
 	vim.g.neovide_floating_blur_amount_x = 2.0
 	vim.g.neovide_floating_blur_amount_y = 2.0
 	vim.g.neovide_transparency = 1.0
+	vim.g.neovide_scroll_animation_length = 0.2
+
+	-- disable cursor animations
+	vim.g.neovide_cursor_animation_length = 0
+	vim.g.neovide_cursor_trail_size = 0
+	vim.g.neovide_cursor_animate_in_insert_mode = false
+	vim.g.neovide_cursor_animate_command_line = false
 end
